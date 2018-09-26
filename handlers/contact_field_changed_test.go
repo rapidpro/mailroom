d45aa86163a70b3cada5420e0f86db3575ed34e0
Nic Pottier <nicpottier@gmail.com>
2018-09-25 20:26:42 -0500

    add test for number setting

--- handlers/contact_field_changed_test.go
+++ handlers/contact_field_changed_test.go
@@ -8,18 +8,23 @@ import (
 	"github.com/nyaruka/goflow/flows"
 	"github.com/nyaruka/goflow/flows/events"
 	"github.com/nyaruka/mailroom/models"
+	"github.com/nyaruka/mailroom/testsuite"
+	"github.com/shopspring/decimal"
 )
 
 func TestContactFieldChanged(t *testing.T) {
+	testsuite.Reset()
+
 	genderUUID := models.FieldUUID("0d345143-1863-4688-93c9-e392c0e77954")
 	gender := assets.NewFieldReference("gender", "Gender")
 
 	ageUUID := models.FieldUUID("042c13fe-e0b1-4426-9192-2681758c2619")
 	age := assets.NewFieldReference("age", "Age")
 
-	unknown := assets.NewFieldReference("unknown", "unknown")
+	unknown := assets.NewFieldReference("unknown", "unknown ")
 
-	// TODO: add other field types when done
+	// TODO: add other field types (state, district, ward, datetime)
+	decimal30 := types.NewXNumber(decimal.New(30, 0))
 
 	tcs := []EventTestCase{
 		EventTestCase{
@@ -32,6 +37,7 @@ func TestContactFieldChanged(t *testing.T) {
 				Evan: []flows.Event{
 					events.NewContactFieldChangedEvent(gender, &flows.Value{Text: types.NewXText("Male")}),
 					events.NewContactFieldChangedEvent(gender, &flows.Value{}),
+					events.NewContactFieldChangedEvent(age, &flows.Value{Text: types.NewXText("30"), Number: &decimal30}),
 				},
 				Bob: []flows.Event{
 					events.NewContactFieldChangedEvent(gender, &flows.Value{}),
@@ -57,11 +63,15 @@ func TestContactFieldChanged(t *testing.T) {
 					Count: 1,
 				},
 				SQLAssertion{
+					SQL:   `select count(*) from contacts_contact where id = $1 AND fields->$2 = '{"text":"30", "number": 30}'::jsonb`,
+					Args:  []interface{}{Evan, ageUUID},
+					Count: 1,
+				},
+				SQLAssertion{
 					SQL:   `select count(*) from contacts_contact where id = $1 AND fields->$2 = '{"text":"Male"}'::jsonb`,
 					Args:  []interface{}{Bob, genderUUID},
 					Count: 1,
 				},
-				// TODO: this should have numeric values in our test
 				SQLAssertion{
 					SQL:   `select count(*) from contacts_contact where id = $1 AND fields->$2 = '{"text":"Old"}'::jsonb`,
 					Args:  []interface{}{Bob, ageUUID},
