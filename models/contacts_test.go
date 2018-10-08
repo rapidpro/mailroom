4c3145f4e5a5fddc4c9cb41863989b9e2cb341da
Nic Pottier <nicpottier@gmail.com>
2018-10-08 09:27:01 -0500

    wip

---
+++ models/contacts_test.go
@@ -0,0 +1,95 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/nyaruka/goflow/excellent/types"
+	"github.com/nyaruka/goflow/flows"
+	"github.com/nyaruka/goflow/flows/engine"
+	"github.com/nyaruka/mailroom/testsuite"
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestContacts(t *testing.T) {
+	ctx := testsuite.CTX()
+	db := testsuite.DB()
+
+	org, err := GetOrgAssets(ctx, db, 1)
+	assert.NoError(t, err)
+
+	session, err := engine.NewSessionAssets(org)
+	assert.NoError(t, err)
+
+	db.MustExec(
+		`INSERT INTO contacts_contacturn(org_id, contact_id, scheme, path, identity, priority) 
+		                          VALUES(1, 80, 'whatsapp', '250788373373', 'whatsapp:250788373373', 100)`)
+
+	db.MustExec(`DELETE FROM contacts_contacturn WHERE contact_id = 43`)
+	db.MustExec(`DELETE FROM contacts_contactgroup_contacts WHERE contact_id = 43`)
+	db.MustExec(`UPDATE contacts_contact SET is_active = FALSE WHERE id = 82`)
+
+	modelContacts, err := LoadContacts(ctx, db, org, []flows.ContactID{1, 42, 43, 80, 82})
+	assert.NoError(t, err)
+	assert.Equal(t, 3, len(modelContacts))
+
+	// convert to goflow contacts
+	contacts := make([]*flows.Contact, len(modelContacts))
+	for i := range modelContacts {
+		contacts[i], err = modelContacts[i].FlowContact(org, session)
+		assert.NoError(t, err)
+	}
+
+	if len(contacts) == 3 {
+		assert.Equal(t, "Cathy Quincy", contacts[0].Name())
+		assert.Equal(t, len(contacts[0].URNs()), 1)
+		assert.Equal(t, contacts[0].URNs()[0].String(), "tel:+250700000001?id=42&priority=50")
+		assert.Equal(t, 5, contacts[0].Groups().Length())
+
+		assert.Equal(t, flows.LocationPath("Nigeria > Sokoto"), contacts[0].Fields()["state"].TypedValue())
+		assert.Equal(t, flows.LocationPath("Nigeria > Sokoto > Yabo > Kilgori"), contacts[0].Fields()["ward"].TypedValue())
+		assert.Equal(t, types.NewXText("F"), contacts[0].Fields()["gender"].TypedValue())
+		assert.Equal(t, (*flows.FieldValue)(nil), contacts[0].Fields()["age"])
+
+		assert.Equal(t, "Dave Jameson", contacts[1].Name())
+		assert.Equal(t, types.NewXNumber(decimal.RequireFromString("30")), contacts[1].Fields()["age"].TypedValue())
+		assert.Equal(t, 0, len(contacts[1].URNs()))
+		assert.Equal(t, 0, contacts[1].Groups().Length())
+
+		assert.Equal(t, "Cathy Roberts", contacts[2].Name())
+		assert.NotNil(t, contacts[2].Fields()["joined"].TypedValue())
+		assert.Equal(t, 2, len(contacts[2].URNs()))
+		assert.Equal(t, contacts[2].URNs()[0].String(), "whatsapp:250788373373?id=10044&priority=100")
+		assert.Equal(t, contacts[2].URNs()[1].String(), "tel:+250700000039?id=82&priority=50")
+		assert.Equal(t, 2, contacts[2].Groups().Length())
+	}
+
+	// change cathy to have a preferred URN and channel of our telephone
+	channel := org.ChannelByID(ChannelID(2))
+	err = modelContacts[2].UpdatePreferredURNAndChannel(ctx, db, URNID(82), channel)
+	assert.NoError(t, err)
+
+	cathy, err := modelContacts[2].FlowContact(org, session)
+	assert.NoError(t, err)
+	assert.Equal(t, cathy.URNs()[0].String(), "tel:+250700000039?channel=c534272e-817d-4a78-a70c-f21df34407f8&channel_id=2&id=82&priority=101")
+	assert.Equal(t, cathy.URNs()[1].String(), "whatsapp:250788373373?id=10044&priority=100")
+
+	// add another tel urn to cathy
+	db.MustExec(
+		`INSERT INTO contacts_contacturn(org_id, contact_id, scheme, path, identity, priority) 
+		                          VALUES(1, 80, 'tel', '+250788373393', 'tel:+250788373373', 10)`)
+
+	// reload the contact
+	modelContacts, err = LoadContacts(ctx, db, org, []flows.ContactID{80})
+	assert.NoError(t, err)
+
+	// set our preferred channel again
+	err = modelContacts[0].UpdatePreferredURNAndChannel(ctx, db, URNID(82), channel)
+	assert.NoError(t, err)
+
+	cathy, err = modelContacts[0].FlowContact(org, session)
+	assert.NoError(t, err)
+	assert.Equal(t, cathy.URNs()[0].String(), "tel:+250700000039?channel=c534272e-817d-4a78-a70c-f21df34407f8&channel_id=2&id=82&priority=101")
+	assert.Equal(t, cathy.URNs()[1].String(), "whatsapp:250788373373?id=10044&priority=100")
+	assert.Equal(t, cathy.URNs()[2].String(), "tel:+250788373393?channel=c534272e-817d-4a78-a70c-f21df34407f8&channel_id=2&id=10045&priority=10")
+}
---
+++ models/env.go
@@ -0,0 +1,107 @@
+package models
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/juju/errors"
+	"github.com/nyaruka/goflow/utils"
+)
+
+type OrgID int
+
+// Org is mailroom's type for RapidPro orgs. It also implements the utils.Environment interface for GoFlow
+type Org struct {
+	id  OrgID
+	env utils.Environment
+}
+
+// ID returns the id of the org
+func (o *Org) ID() OrgID { return o.id }
+
+// DateFormat returns the date format for this org
+func (o *Org) DateFormat() utils.DateFormat { return o.env.DateFormat() }
+
+// TimeFormat returns the time format for this org
+func (o *Org) TimeFormat() utils.TimeFormat { return o.env.TimeFormat() }
+
+// Timezone returns the timezone for this org
+func (o *Org) Timezone() *time.Location { return o.env.Timezone() }
+
+// DefaultLanguage returns the primary language for this org
+func (o *Org) DefaultLanguage() utils.Language { return o.env.DefaultLanguage() }
+
+// AllowedLanguages returns the list of supported languages for this org
+func (o *Org) AllowedLanguages() []utils.Language { return o.env.AllowedLanguages() }
+
+// RedactionPolicy returns the redaction policy (are we anonymous) for this org
+func (o *Org) RedactionPolicy() utils.RedactionPolicy { return o.env.RedactionPolicy() }
+
+// Now returns the current time in the current timezone for this org
+func (o *Org) Now() time.Time { return o.env.Now() }
+
+// Extension returns the extension for this org
+func (o *Org) Extension(name string) json.RawMessage { return o.env.Extension(name) }
+
+// Equal return whether we are equal to the passed in environment
+func (o *Org) Equal(env utils.Environment) bool { return o.env.Equal(env) }
+
+// MarshalJSON is our custom marshaller so that our inner env get output
+func (o *Org) MarshalJSON() ([]byte, error) {
+	return json.Marshal(o.env)
+}
+
+// loadOrg loads the org for the passed in id, returning any error encountered
+func loadOrg(ctx context.Context, db sqlx.Queryer, orgID OrgID) (*Org, error) {
+	org := &Org{}
+	var orgJSON json.RawMessage
+	rows, err := db.Query(selectOrgEnvironment, orgID)
+	if err != nil {
+		return nil, errors.Annotatef(err, "error loading org: %d", orgID)
+	}
+	defer rows.Close()
+
+	rows.Next()
+	err = rows.Scan(&org.id, &orgJSON)
+	if err != nil {
+		return nil, errors.Annotatef(err, "error scanning org: %d", orgID)
+	}
+
+	org.env, err = utils.ReadEnvironment(orgJSON)
+	if err != nil {
+		return nil, errors.Annotatef(err, "error unmarshalling org json: %s", orgJSON)
+	}
+
+	return org, nil
+}
+
+const selectOrgEnvironment = `
+SELECT id, ROW_TO_JSON(o) FROM (SELECT
+	id,
+	(SELECT CASE date_format
+		WHEN 'D' THEN 'DD-MM-YYYY'
+		WHEN 'M' THEN 'MM-DD-YYYY'
+	END) date_format,
+	'tt:mm' as time_format,
+	timezone,
+	(SELECT CASE is_anon
+		WHEN TRUE THEN 'urns'
+		WHEN FALSE THEN 'none'
+	END) redaction_policy,
+	COALESCE(language, '') as default_language,
+	(SELECT 
+		CASE
+			WHEN language IS NOT NULL THEN ARRAY_PREPEND(language, ARRAY_AGG(l.iso_code)) 
+			ELSE ARRAY_AGG(l.iso_code) 
+		END 
+		FROM (
+			SELECT iso_code FROM orgs_language WHERE org_id = o.id
+		) 
+	l) allowed_languages
+	FROM 
+		orgs_org o
+	WHERE
+		o.id = $1
+) o`
