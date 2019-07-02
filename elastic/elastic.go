1646b8c1e938ed18495e33d19857fe5f1e051d5c
Nic Pottier <nicpottier@gmail.com>
2019-07-02 12:12:44 -0700

    parsing into elastic queries from contactql

---
+++ elastic/elastic.go
@@ -0,0 +1,260 @@
+package elastic
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/nyaruka/goflow/contactql"
+	"github.com/nyaruka/goflow/utils"
+	q "github.com/olivere/elastic"
+	"github.com/pkg/errors"
+	"github.com/shopspring/decimal"
+)
+
+type FieldCategory string
+
+const ContactAttribute = FieldCategory("attribute")
+const ContactField = FieldCategory("field")
+const Scheme = FieldCategory("scheme")
+const Unavailable = FieldCategory("unavailable")
+const Implicit = FieldCategory("implicit")
+
+type FieldType string
+
+const TextType = FieldType("text")
+const DateTimeType = FieldType("datetime")
+const NumberType = FieldType("number")
+const StateType = FieldType("state")
+const DistrictType = FieldType("district")
+const WardType = FieldType("ward")
+
+type Field struct {
+	Key      string
+	Category FieldCategory
+	Type     FieldType
+	UUID     utils.UUID
+}
+
+// FieldRegistry provides an interface for looking up queryable fields
+type FieldRegistry interface {
+	LookupField(key string) *Field
+}
+
+func ToElasticQuery(env utils.Environment, registry FieldRegistry, node contactql.QueryNode) (q.Query, error) {
+	switch n := node.(type) {
+	case *contactql.ContactQuery:
+		return ToElasticQuery(env, registry, n.Root())
+	case *contactql.BoolCombination:
+		return BoolCombinationToElasticQuery(env, registry, n)
+	case *contactql.Condition:
+		return ConditionToElasticQuery(env, registry, n)
+	default:
+		return nil, errors.Errorf("unknown type converting to elastic query: %v", n)
+	}
+}
+
+func BoolCombinationToElasticQuery(env utils.Environment, registry FieldRegistry, combination *contactql.BoolCombination) (q.Query, error) {
+	queries := make([]q.Query, len(combination.Children()))
+	for i, child := range combination.Children() {
+		childQuery, err := ToElasticQuery(env, registry, child)
+		if err != nil {
+			return nil, errors.Wrapf(err, "error evaluating child query")
+		}
+		queries[i] = childQuery
+	}
+
+	if combination.Operator() == contactql.BoolOperatorAnd {
+		return q.NewBoolQuery().Must(queries...), nil
+	}
+
+	return q.NewBoolQuery().Should(queries...), nil
+}
+
+func ConditionToElasticQuery(env utils.Environment, registry FieldRegistry, c *contactql.Condition) (q.Query, error) {
+	field := registry.LookupField(c.Key())
+	if field == nil {
+		return nil, errors.Errorf("unable to find field: %s", c.Key())
+	}
+
+	var query q.Query
+
+	if field.Category == Implicit {
+		number, _ := strconv.Atoi(c.Value())
+
+		if field.Key == "name_tel" {
+			if number != 0 {
+				return q.NewNestedQuery("urns", q.NewMatchPhraseQuery("urns.path", number)), nil
+			}
+			return q.NewMatchQuery("name", c.Value()), nil
+
+		} else if field.Key == "name_id" {
+			if number != 0 {
+				return q.NewTermQuery("ids", number), nil
+			}
+			return q.NewMatchQuery("name", c.Value()), nil
+		} else {
+			return nil, fmt.Errorf("unknown implicit field key: %s", field.Key)
+		}
+	} else if field.Category == ContactField {
+		fieldQuery := q.NewTermQuery("fields.field", field.UUID)
+
+		if field.Type == TextType {
+			value := strings.ToLower(c.Value())
+			if c.Comparator() == "=" {
+				query = q.NewTermQuery("fields.text", value)
+			} else if c.Comparator() == "!=" {
+				query = q.NewBoolQuery().Must(
+					fieldQuery,
+					q.NewTermQuery("fields.text", value),
+					q.NewExistsQuery("fields.text"),
+				)
+				return q.NewBoolQuery().MustNot(q.NewNestedQuery("fields", query)), nil
+			} else {
+				return nil, fmt.Errorf("unknown text comparator: %s", c.Comparator())
+			}
+
+			return q.NewBoolQuery().Must(fieldQuery, query), nil
+
+		} else if field.Type == NumberType {
+			value, err := decimal.NewFromString(c.Value())
+			if err != nil {
+				return nil, errors.Errorf("can't convert '%s' to a number", c.Value())
+			}
+
+			if c.Comparator() == "=" {
+				query = q.NewMatchQuery("fields.number", value)
+			} else if c.Comparator() == ">" {
+				query = q.NewRangeQuery("fields.number").Gt(value)
+			} else if c.Comparator() == ">=" {
+				query = q.NewRangeQuery("fields.number").Gte(value)
+			} else if c.Comparator() == "<" {
+				query = q.NewRangeQuery("fields_number").Lt(value)
+			} else if c.Comparator() == "<=" {
+				query = q.NewRangeQuery("fields.number").Lte(value)
+			} else {
+				return nil, fmt.Errorf("unknown number comparator: %s", c.Comparator())
+			}
+
+			return q.NewBoolQuery().Must(fieldQuery, query), nil
+
+		} else if field.Type == DateTimeType {
+			value, err := utils.DateTimeFromString(env, c.Value(), false)
+			if err != nil {
+				return nil, errors.Wrapf(err, "unable to parse datetime: %s", c.Value())
+			}
+			start, end := utils.DateToUTCRange(value, value.Location())
+
+			if c.Comparator() == "=" {
+				query = q.NewRangeQuery("fields.datetime").Gte(start).Lt(end)
+			} else if c.Comparator() == ">" {
+				query = q.NewRangeQuery("fields.datetime").Gte(end)
+			} else if c.Comparator() == ">=" {
+				query = q.NewRangeQuery("fields.datetime").Gte(start)
+			} else if c.Comparator() == "<" {
+				query = q.NewRangeQuery("fields.datetime").Lt(start)
+			} else if c.Comparator() == "<=" {
+				query = q.NewRangeQuery("fields.datetime").Lt(end)
+			} else {
+				return nil, fmt.Errorf("unknown datetime comparator: %s", c.Comparator())
+			}
+
+			return q.NewBoolQuery().Must(fieldQuery, query), nil
+
+		} else if field.Type == StateType || field.Type == DistrictType || field.Type == WardType {
+			value := strings.ToLower(c.Value())
+			var name = ""
+
+			if field.Type == WardType {
+				name = "fields.ward"
+			} else if field.Type == DistrictType {
+				name = "fields.district"
+			} else if field.Type == StateType {
+				name = "fields.state"
+			}
+			name += "_keyword"
+
+			if c.Comparator() == "=" {
+				query = q.NewTermQuery(name, value)
+			} else if c.Comparator() == "!=" {
+				return q.NewBoolQuery().MustNot(
+					q.NewNestedQuery("fields",
+						q.NewBoolQuery().Must(
+							q.NewTermQuery(name, value),
+							q.NewExistsQuery(name),
+						),
+					),
+				), nil
+			} else {
+				return nil, fmt.Errorf("unknown location comparator: %s", c.Comparator())
+			}
+
+			return q.NewBoolQuery().Must(fieldQuery, query), nil
+		} else {
+			return nil, fmt.Errorf("unknown contact field type: %s", field.Type)
+		}
+	} else if field.Category == ContactAttribute {
+		value := strings.ToLower(c.Value())
+
+		if field.Key == "name" {
+			if c.Comparator() == "=" {
+				return q.NewTermQuery("name.keyword", c.Value()), nil
+			} else if c.Comparator() == "~" {
+				return q.NewMatchQuery("name", value), nil
+			} else if c.Comparator() == "!=" {
+				return q.NewBoolQuery().MustNot(q.NewTermQuery("name.keyword", c.Value())), nil
+			} else {
+				return nil, fmt.Errorf("unknown name query comparator: %s", c.Comparator())
+			}
+		} else if field.Key == "id" {
+			if c.Comparator() == "=" {
+				return q.NewTermQuery("ids", value), nil
+			}
+			return nil, fmt.Errorf("unknown comparator for id: %s", c.Comparator())
+		} else if field.Key == "language" {
+			if c.Comparator() == "=" {
+				return q.NewTermQuery("language", value), nil
+			} else if c.Comparator() == "!=" {
+				return q.NewBoolQuery().MustNot(q.NewTermQuery("language", value)), nil
+			} else {
+				return nil, fmt.Errorf("unknown language comparator: %s", c.Comparator())
+			}
+		} else if field.Key == "created_on" {
+			value, err := utils.DateTimeFromString(env, c.Value(), false)
+			if err != nil {
+				return nil, errors.Wrapf(err, "unable to parse datetime: %s", c.Value())
+			}
+			start, end := utils.DateToUTCRange(value, value.Location())
+
+			if c.Comparator() == "=" {
+				return q.NewRangeQuery("created_on").Gte(start).Lt(end), nil
+			} else if c.Comparator() == ">" {
+				return q.NewRangeQuery("created_on").Gte(end), nil
+			} else if c.Comparator() == ">=" {
+				return q.NewRangeQuery("created_on").Gte(start), nil
+			} else if c.Comparator() == "<" {
+				return q.NewRangeQuery("created_on").Lt(start), nil
+			} else if c.Comparator() == "<=" {
+				return q.NewRangeQuery("created_on").Lt(end), nil
+			} else {
+				return nil, fmt.Errorf("unknown created_on comparator: %s", c.Comparator())
+			}
+		} else {
+			return nil, fmt.Errorf("unknown contact attribute: %s", field.Key)
+		}
+	} else if field.Category == Scheme {
+		value := strings.ToLower(c.Value())
+
+		if c.Comparator() == "=" {
+			return q.NewNestedQuery("urns", q.NewTermQuery("urns.path.keyword", value)), nil
+		} else if c.Comparator() == "!=" {
+			return q.NewNestedQuery("urns", q.NewMatchPhraseQuery("urns.path", value)), nil
+		} else {
+			return nil, fmt.Errorf("unknown scheme comparator: %s", c.Comparator())
+		}
+	} else if field.Category == Unavailable {
+		return q.NewTermQuery("ids", "-1"), nil
+	}
+
+	return nil, errors.Errorf("unknown category type: %s", field.Category)
+}
---
+++ elastic/elastic_test.go
@@ -0,0 +1,120 @@
+package elastic
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+	"time"
+
+	"github.com/nyaruka/goflow/contactql"
+	"github.com/nyaruka/goflow/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+type MockRegistry struct {
+	Fields map[string]*Field
+	IsAnon bool
+}
+
+func (r *MockRegistry) LookupField(key string) *Field {
+	field := r.Fields[key]
+	if field == nil {
+		return field
+	}
+
+	if field.Category == Scheme && r.IsAnon {
+		return &Field{key, Unavailable, TextType, ""}
+	}
+
+	if field.Category == Implicit && r.IsAnon {
+		return &Field{"name_id", Implicit, TextType, ""}
+	}
+
+	return field
+}
+
+func TestElasticQuery(t *testing.T) {
+	registry := &MockRegistry{
+		Fields: map[string]*Field{
+			"name":       &Field{"name", ContactAttribute, TextType, ""},
+			"id":         &Field{"id", ContactAttribute, TextType, ""},
+			"language":   &Field{"language", ContactAttribute, TextType, ""},
+			"created_on": &Field{"created_on", ContactAttribute, DateTimeType, ""},
+
+			"age":      &Field{"age", ContactField, NumberType, "6b6a43fa-a26d-4017-bede-328bcdd5c93b"},
+			"color":    &Field{"color", ContactField, TextType, "ecc7b13b-c698-4f46-8a90-24a8fab6fe34"},
+			"dob":      &Field{"dob", ContactField, DateTimeType, "cbd3fc0e-9b74-4207-a8c7-248082bb4572"},
+			"state":    &Field{"state", ContactField, StateType, "67663ad1-3abc-42dd-a162-09df2dea66ec"},
+			"district": &Field{"district", ContactField, DistrictType, "54c72635-d747-4e45-883c-099d57dd998e"},
+			"ward":     &Field{"ward", ContactField, WardType, "fde8f740-c337-421b-8abb-83b954897c80"},
+
+			"tel":      &Field{"tel", Scheme, TextType, ""},
+			"whatsapp": &Field{"whatsapp", Scheme, TextType, ""},
+
+			"*": &Field{"name_tel", Implicit, TextType, ""},
+		},
+	}
+
+	type TestCase struct {
+		Label  string          `json:"label"`
+		Search string          `json:"search"`
+		Query  json.RawMessage `json:"query"`
+		Error  string          `json:"error"`
+		IsAnon bool            `json:"is_anon"`
+	}
+
+	tcs := make([]TestCase, 0, 20)
+	tcJSON, err := ioutil.ReadFile("testdata/elastic_test.json")
+	assert.NoError(t, err)
+
+	err = json.Unmarshal(tcJSON, &tcs)
+	assert.NoError(t, err)
+
+	ny, _ := time.LoadLocation("America/New_York")
+	env := utils.NewEnvironmentBuilder().WithTimezone(ny).Build()
+
+	for _, tc := range tcs {
+		registry.IsAnon = tc.IsAnon
+
+		parsed, err := contactql.ParseQuery(tc.Search)
+		assert.NoError(t, err, "%s: error received parsing: ", tc.Label, tc.Search)
+
+		query, err := ToElasticQuery(env, registry, parsed)
+
+		if tc.Error != "" {
+			assert.Error(t, err, "%s: error not received converting to elastic: %s", tc.Label, tc.Search)
+			if err != nil {
+				assert.Contains(t, err.Error(), tc.Error)
+			}
+			continue
+		}
+
+		assert.NoError(t, err, "%s: error received converting to elastic: %s", tc.Label, tc.Search)
+		if err != nil {
+			continue
+		}
+
+		assert.NotNil(t, query, tc.Label)
+		if query == nil {
+			continue
+		}
+
+		source, err := query.Source()
+		assert.NoError(t, err, tc.Label)
+		if err != nil {
+			continue
+		}
+
+		asJSON, err := json.Marshal(source)
+		assert.NoError(t, err, tc.Label)
+		if err != nil {
+			continue
+		}
+
+		compacted := &bytes.Buffer{}
+		json.Compact(compacted, tc.Query)
+
+		assert.Equal(t, compacted.String(), string(asJSON), "%s: generated query does not match for: %s", tc.Label, tc.Search)
+	}
+}
