e3bd8c488d6b6e3b43671135886a6bbe85b137cc
Rowan Seymour <rowanseymour@gmail.com>
2019-08-27 13:40:09 -0500

    Update to goflow v0.49.0

--- goflow/engine.go
+++ goflow/engine.go
@@ -1,18 +1,33 @@
 package goflow
 
 import (
+	"crypto/tls"
+	"net/http"
 	"sync"
+	"time"
 
 	"github.com/nyaruka/goflow/flows"
 	"github.com/nyaruka/goflow/flows/engine"
+	"github.com/nyaruka/goflow/flows/engine/webhooks"
 	"github.com/nyaruka/mailroom/config"
 )
 
-// Engine returns the global engine for use in mailroom
+// Engine returns the global engine instance for use in mailroom
 func Engine() flows.Engine {
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			MaxIdleConns:    10,
+			IdleConnTimeout: 30 * time.Second,
+			TLSClientConfig: &tls.Config{
+				Renegotiation: tls.RenegotiateOnceAsClient, // support single TLS renegotiation
+			},
+		},
+		Timeout: time.Duration(15 * time.Second),
+	}
+
 	engInit.Do(func() {
 		eng = engine.NewBuilder().
-			WithDefaultUserAgent("RapidProMailroom/" + config.Mailroom.Version).
+			WithWebhookService(webhooks.NewService(httpClient, "RapidProMailroom/"+config.Mailroom.Version, 10000)).
 			WithMaxStepsPerSprint(config.Mailroom.MaxStepsPerSprint).
 			Build()
 	})
