3ada7d28f3c6fb7733ea2f17ab90b2e27ce1d2cc
Matheus <matheus.msa27@gmail.com>
2019-11-04 18:14:36 -0300

    generate a new dump file and change ClassifierTypeBothub to 'bothub'

--- models/classifiers.go
+++ models/classifiers.go
@@ -27,7 +27,7 @@ const (
 	// Our classifier types
 	ClassifierTypeWit    = "wit"
 	ClassifierTypeLuis   = "luis"
-	ClassifierTypeBothub = "bh"
+	ClassifierTypeBothub = "bothub"
 
 	// Wit.ai config options
 	WitConfigAccessToken = "access_token"
