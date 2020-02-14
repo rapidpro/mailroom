5be75a7ad3dec6f38f2d2359d813cc99bc739861
Rowan Seymour <rowanseymour@gmail.com>
2020-02-14 10:45:22 -0500

    Convert flow and expression endpoint testing to be snapshot based

---
+++ testsuite/server.go
@@ -0,0 +1,86 @@
+package testsuite
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/nyaruka/goflow/test"
+	"github.com/nyaruka/goflow/utils/uuids"
+	"github.com/nyaruka/mailroom/config"
+	"github.com/nyaruka/mailroom/web"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type ServerTestCase struct {
+	URL      string
+	Method   string
+	Files    string
+	Status   int
+	Response string
+}
+
+func RunServerTestCases(t *testing.T, tcs []ServerTestCase) {
+	Reset()
+	ctx := CTX()
+	db := DB()
+	rp := RP()
+	wg := &sync.WaitGroup{}
+
+	server := web.NewServer(ctx, config.Mailroom, db, rp, nil, nil, wg)
+	server.Start()
+
+	// give our server time to start
+	time.Sleep(time.Second)
+
+	defer server.Stop()
+	defer uuids.SetGenerator(uuids.DefaultGenerator)
+
+	for _, tc := range tcs {
+		uuids.SetGenerator(uuids.NewSeededGenerator(123456))
+		time.Sleep(1 * time.Second)
+
+		testID := fmt.Sprintf("%s %s %s", tc.Method, tc.URL, tc.Files)
+		var requestBody io.Reader
+		var err error
+
+		if tc.Files != "" {
+			requestPath := "testdata/" + tc.Files + ".json"
+			requestBody, err = os.Open(requestPath)
+			require.NoError(t, err, "unable to open %s", requestPath)
+		}
+
+		req, err := http.NewRequest(tc.Method, "http://localhost:8090"+tc.URL, requestBody)
+		assert.NoError(t, err, "error creating request in %s", testID)
+
+		resp, err := http.DefaultClient.Do(req)
+		assert.NoError(t, err, "error making request in %s", testID)
+
+		content, err := ioutil.ReadAll(resp.Body)
+		require.NoError(t, err, "error reading body in %s", testID)
+
+		assert.Equal(t, tc.Status, resp.StatusCode, "unexpected status in %s (response=%s)", testID, content)
+
+		if tc.Files != "" {
+			responsePath := "testdata/" + tc.Files + ".response.json"
+
+			if !test.WriteOutput {
+				expectedRespBody, err := ioutil.ReadFile(responsePath)
+				require.NoError(t, err, "unable to read %s", responsePath)
+
+				test.AssertEqualJSON(t, expectedRespBody, content, "response mismatch in %s", testID)
+			} else {
+				ioutil.WriteFile(responsePath, content, 0666)
+			}
+		} else {
+			test.AssertEqualJSON(t, []byte(tc.Response), content, "response mismatch in %s", testID)
+		}
+	}
+}
--- web/flow/flow_test.go
+++ web/flow/flow_test.go
@@ -1,104 +1,33 @@
 package flow
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"sync"
 	"testing"
-	"time"
 
-	"github.com/nyaruka/goflow/test"
-	"github.com/nyaruka/goflow/utils/uuids"
-	"github.com/nyaruka/mailroom/config"
 	"github.com/nyaruka/mailroom/testsuite"
-	"github.com/nyaruka/mailroom/web"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestServer(t *testing.T) {
-	testsuite.Reset()
-	ctx := testsuite.CTX()
-	db := testsuite.DB()
-	rp := testsuite.RP()
-	wg := &sync.WaitGroup{}
-
-	server := web.NewServer(ctx, config.Mailroom, db, rp, nil, nil, wg)
-	server.Start()
-
-	// give our server time to start
-	time.Sleep(time.Second)
-
-	defer server.Stop()
-	defer uuids.SetGenerator(uuids.DefaultGenerator)
-
-	tcs := []struct {
-		URL             string
-		Method          string
-		BodyFile        string
-		Status          int
-		Response        string
-		ResponseFile    string
-		ResponsePattern string
-	}{
+	tcs := []testsuite.ServerTestCase{
 		{URL: "/mr/flow/migrate", Method: "GET", Status: 405, Response: `{"error": "illegal method: GET"}`},
-		{URL: "/mr/flow/migrate", Method: "POST", BodyFile: "migrate_minimal_v13.json", Status: 200, ResponseFile: "migrate_minimal_v13.response.json"},
-		{URL: "/mr/flow/migrate", Method: "POST", BodyFile: "migrate_minimal_legacy.json", Status: 200, ResponseFile: "migrate_minimal_legacy.response.json"},
-		{URL: "/mr/flow/migrate", Method: "POST", BodyFile: "migrate_legacy_with_version.json", Status: 200, ResponseFile: "migrate_legacy_with_version.response.json"},
-		{URL: "/mr/flow/migrate", Method: "POST", BodyFile: "migrate_invalid_v13.json", Status: 422, Response: `{"error": "unable to read migrated flow: unable to read node: field 'uuid' is required"}`},
+		{URL: "/mr/flow/migrate", Method: "POST", Files: "migrate_minimal_v13", Status: 200},
+		{URL: "/mr/flow/migrate", Method: "POST", Files: "migrate_minimal_legacy", Status: 200},
+		{URL: "/mr/flow/migrate", Method: "POST", Files: "migrate_legacy_with_version", Status: 200},
+		{URL: "/mr/flow/migrate", Method: "POST", Files: "migrate_invalid_v13", Status: 422},
 
 		{URL: "/mr/flow/inspect", Method: "GET", Status: 405, Response: `{"error": "illegal method: GET"}`},
-		{URL: "/mr/flow/inspect", Method: "POST", BodyFile: "inspect_valid_legacy.json", Status: 200, ResponseFile: "inspect_valid_legacy.response.json"},
-		{URL: "/mr/flow/inspect", Method: "POST", BodyFile: "inspect_invalid_legacy.json", Status: 200, ResponseFile: "inspect_invalid_legacy.response.json"},
-		{URL: "/mr/flow/inspect", Method: "POST", BodyFile: "inspect_valid.json", Status: 200, ResponseFile: "inspect_valid.response.json"},
-		{URL: "/mr/flow/inspect", Method: "POST", BodyFile: "inspect_invalid.json", Status: 422, ResponseFile: "inspect_invalid.response.json"},
-		{URL: "/mr/flow/inspect", Method: "POST", BodyFile: "inspect_valid_without_org.json", Status: 200, ResponseFile: "inspect_valid_without_org.response.json"},
-		{URL: "/mr/flow/inspect", Method: "POST", BodyFile: "inspect_invalid_without_org.json", Status: 200, ResponseFile: "inspect_invalid_without_org.response.json"},
-		{URL: "/mr/flow/inspect", Method: "POST", BodyFile: "inspect_legacy_single_msg.json", Status: 200, ResponseFile: "inspect_legacy_single_msg.response.json"},
+		{URL: "/mr/flow/inspect", Method: "POST", Files: "inspect_valid_legacy", Status: 200},
+		{URL: "/mr/flow/inspect", Method: "POST", Files: "inspect_invalid_legacy", Status: 200},
+		{URL: "/mr/flow/inspect", Method: "POST", Files: "inspect_valid", Status: 200},
+		{URL: "/mr/flow/inspect", Method: "POST", Files: "inspect_invalid", Status: 422},
+		{URL: "/mr/flow/inspect", Method: "POST", Files: "inspect_valid_without_org", Status: 200},
+		{URL: "/mr/flow/inspect", Method: "POST", Files: "inspect_invalid_without_org", Status: 200},
+		{URL: "/mr/flow/inspect", Method: "POST", Files: "inspect_legacy_single_msg", Status: 200},
 
 		{URL: "/mr/flow/clone", Method: "GET", Status: 405, Response: `{"error": "illegal method: GET"}`},
-		{URL: "/mr/flow/clone", Method: "POST", BodyFile: "clone_valid.json", Status: 200, ResponsePattern: `"uuid": "1cf84575-ee14-4253-88b6-e3675c04a066"`},
-		{URL: "/mr/flow/clone", Method: "POST", BodyFile: "clone_struct_invalid.json", Status: 422, Response: `{"error": "unable to clone flow: unable to read node: field 'uuid' is required"}`},
-		{URL: "/mr/flow/clone", Method: "POST", BodyFile: "clone_missing_dep_mapping.json", Status: 200, ResponseFile: `clone_missing_dep_mapping.response.json`},
+		{URL: "/mr/flow/clone", Method: "POST", Files: "clone_valid", Status: 200},
+		{URL: "/mr/flow/clone", Method: "POST", Files: "clone_struct_invalid", Status: 422},
+		{URL: "/mr/flow/clone", Method: "POST", Files: "clone_missing_dep_mapping", Status: 200},
 	}
 
-	for _, tc := range tcs {
-		uuids.SetGenerator(uuids.NewSeededGenerator(123456))
-		time.Sleep(1 * time.Second)
-
-		testID := fmt.Sprintf("%s %s %s", tc.Method, tc.URL, tc.BodyFile)
-		var requestBody io.Reader
-		var err error
-
-		if tc.BodyFile != "" {
-			requestBody, err = os.Open("testdata/" + tc.BodyFile)
-			require.NoError(t, err, "unable to open %s", tc.BodyFile)
-		}
-
-		req, err := http.NewRequest(tc.Method, "http://localhost:8090"+tc.URL, requestBody)
-		assert.NoError(t, err, "error creating request in %s", testID)
-
-		resp, err := http.DefaultClient.Do(req)
-		assert.NoError(t, err, "error making request in %s", testID)
-
-		content, err := ioutil.ReadAll(resp.Body)
-		require.NoError(t, err, "error reading body in %s", testID)
-
-		assert.Equal(t, tc.Status, resp.StatusCode, "unexpected status in %s (response=%s)", testID, content)
-
-		if tc.ResponseFile != "" {
-			expectedRespBody, err := ioutil.ReadFile("testdata/" + tc.ResponseFile)
-			require.NoError(t, err, "unable to read %s", tc.ResponseFile)
-
-			test.AssertEqualJSON(t, expectedRespBody, content, "response mismatch in %s", testID)
-		} else if tc.ResponsePattern != "" {
-			assert.Regexp(t, tc.ResponsePattern, string(content), "response mismatch in %s", testID)
-		} else {
-			test.AssertEqualJSON(t, []byte(tc.Response), content, "response mismatch in %s", testID)
-		}
-	}
+	testsuite.RunServerTestCases(t, tcs)
 }
