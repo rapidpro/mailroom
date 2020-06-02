ce5f4e76142c5c52f574612f12842c870d593b05
Rowan Seymour <rowanseymour@gmail.com>
2020-06-02 11:28:51 -0500

    Add endpoint to change a flow language

--- web/flow/flow_test.go
+++ web/flow/flow_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 func TestServer(t *testing.T) {
+	web.RunWebTests(t, "testdata/change_language.json")
 	web.RunWebTests(t, "testdata/clone.json")
 	web.RunWebTests(t, "testdata/inspect.json")
 	web.RunWebTests(t, "testdata/migrate.json")
