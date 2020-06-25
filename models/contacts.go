8eef2a6cebb8357deae4ab900a9d3d0be17a053b
Rowan Seymour <rowanseymour@gmail.com>
2020-06-25 12:02:26 -0500

    Fix detaching URNs

--- models/contacts.go
+++ models/contacts.go
@@ -1083,14 +1083,19 @@ func UpdateContactURNs(ctx context.Context, tx Queryer, org *OrgAssets, changes 
 	// keep track of all our inserts
 	inserts := make([]interface{}, 0, len(changes))
 
-	// and updates
+	// and updates to URNs
 	updates := make([]interface{}, 0, len(changes))
+
+	contactIDs := make([]ContactID, 0)
+	updatedURNIDs := make([]URNID, 0)
 
 	// identities we are inserting
 	identities := make([]string, 0, 1)
 
 	// for each of our changes (one per contact)
 	for _, change := range changes {
+		contactIDs = append(contactIDs, change.ContactID)
+
 		// priority for each contact starts at 1000
 		priority := topURNPriority
 
@@ -1100,15 +1105,17 @@ func UpdateContactURNs(ctx context.Context, tx Queryer, org *OrgAssets, changes 
 			channelID := GetURNChannelID(org, urn)
 
 			// do we have an id?
-			urnID := GetURNInt(urn, "id")
+			urnID := URNID(GetURNInt(urn, "id"))
 
 			if urnID > 0 {
 				// if so, this is a URN update
 				updates = append(updates, &urnUpdate{
-					URNID:     URNID(urnID),
+					URNID:     urnID,
 					ChannelID: channelID,
 					Priority:  priority,
 				})
+
+				updatedURNIDs = append(updatedURNIDs, urnID)
 			} else {
 				// new URN, add it instead
 				inserts = append(inserts, &urnInsert{
@@ -1134,6 +1141,17 @@ func UpdateContactURNs(ctx context.Context, tx Queryer, org *OrgAssets, changes 
 	err := BulkSQL(ctx, "updating contact urns", tx, updateContactURNsSQL, updates)
 	if err != nil {
 		return errors.Wrapf(err, "error updating urns")
+	}
+
+	// then detach any URNs that weren't updated (the ones we're not keeping)
+	_, err = tx.ExecContext(
+		ctx,
+		`UPDATE contacts_contacturn SET contact_id = NULL WHERE contact_id = ANY($1) AND id != ALL($2)`,
+		pq.Array(contactIDs),
+		pq.Array(updatedURNIDs),
+	)
+	if err != nil {
+		return errors.Wrapf(err, "error detaching urns")
 	}
 
 	if len(inserts) > 0 {
