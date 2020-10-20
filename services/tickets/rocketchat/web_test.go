8705272d397ac24b25d07634a1bbfe6d21249581
Allan Lima <allancslima@gmail.com>
2020-10-20 15:09:55 -0300

    Use the new testdata.InsertOpenTicket in RocketChat ticketer

--- services/tickets/rocketchat/web_test.go
+++ services/tickets/rocketchat/web_test.go
@@ -3,6 +3,7 @@ package rocketchat_test
 import (
 	"github.com/nyaruka/mailroom/core/models"
 	"github.com/nyaruka/mailroom/testsuite"
+	"github.com/nyaruka/mailroom/testsuite/testdata"
 	"github.com/nyaruka/mailroom/web"
 	"testing"
 )
@@ -12,8 +13,7 @@ func TestEventCallback(t *testing.T) {
 	db := testsuite.DB()
 
 	// create a rocketchat ticket for Cathy
-	db.MustExec(`INSERT INTO tickets_ticket(id, uuid, org_id, contact_id, ticketer_id, status, subject, body, opened_on, modified_on)
-	VALUES(1, 'c69f103c-db64-4481-815b-1112890419ef', $1, $2, $3, 'O', 'Need help', 'Have you seen my cookies?', NOW(), NOW())`, models.Org1, models.CathyID, models.RocketChatID)
+	testdata.InsertOpenTicket(t, db, models.Org1, models.CathyID, models.RocketChatID, "c69f103c-db64-4481-815b-1112890419ef", "Need help", "Have you seen my cookies?", "1234")
 
 	web.RunWebTests(t, "testdata/event_callback.json")
 }
