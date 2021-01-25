22891603dc4f027cd0381fce23e709d277762970
Rowan Seymour <rowanseymour@gmail.com>
2021-01-25 16:18:25 -0500

    Fix non-deterministic ordering of org languages

--- core/models/orgs.go
+++ core/models/orgs.go
@@ -264,7 +264,7 @@ SELECT ROW_TO_JSON(o) FROM (SELECT
 	(SELECT CASE is_anon WHEN TRUE THEN 'urns' WHEN FALSE THEN 'none' END) AS redaction_policy,
 	$2::int AS max_value_length,
 	(SELECT iso_code FROM orgs_language WHERE id = o.primary_language_id) AS default_language,
-	(SELECT ARRAY_AGG(iso_code) FROM orgs_language WHERE org_id = o.id) AS allowed_languages,
+	(SELECT ARRAY_AGG(iso_code ORDER BY iso_code ASC) FROM orgs_language WHERE org_id = o.id) AS allowed_languages,
 	COALESCE(
 		(
 			SELECT
