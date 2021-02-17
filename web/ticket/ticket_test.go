7e9b2be828c261a45505504737419bb1ba6e83b6
Eric Newcomer <ericnewcomer@gmail.com>
2021-02-17 14:07:01 -0800

    Revert general test across ticketers

--- web/ticket/ticket_test.go
+++ web/ticket/ticket_test.go
@@ -17,8 +17,8 @@ func TestTicketClose(t *testing.T) {
 	testsuite.Reset()
 	db := testsuite.DB()
 
-	// create 2 open tickets and 1 closed one for Cathy
-	testdata.InsertOpenTicket(t, db, models.Org1, models.CathyID, models.ZendeskID, flows.TicketUUID(uuids.New()), "Need help", "Have you seen my cookies?", "17")
+	// create 2 open tickets and 1 closed one for Cathy across two different ticketers
+	testdata.InsertOpenTicket(t, db, models.Org1, models.CathyID, models.MailgunID, flows.TicketUUID(uuids.New()), "Need help", "Have you seen my cookies?", "17")
 	testdata.InsertOpenTicket(t, db, models.Org1, models.CathyID, models.ZendeskID, flows.TicketUUID(uuids.New()), "More help", "Have you seen my cookies?", "21")
 	testdata.InsertClosedTicket(t, db, models.Org1, models.CathyID, models.ZendeskID, flows.TicketUUID(uuids.New()), "Old question", "Have you seen my cookies?", "34")
 
@@ -30,7 +30,7 @@ func TestTicketReopen(t *testing.T) {
 	db := testsuite.DB()
 
 	// create 2 closed tickets and 1 open one for Cathy
-	testdata.InsertClosedTicket(t, db, models.Org1, models.CathyID, models.ZendeskID, flows.TicketUUID(uuids.New()), "Need help", "Have you seen my cookies?", "17")
+	testdata.InsertClosedTicket(t, db, models.Org1, models.CathyID, models.MailgunID, flows.TicketUUID(uuids.New()), "Need help", "Have you seen my cookies?", "17")
 	testdata.InsertClosedTicket(t, db, models.Org1, models.CathyID, models.ZendeskID, flows.TicketUUID(uuids.New()), "More help", "Have you seen my cookies?", "21")
 	testdata.InsertOpenTicket(t, db, models.Org1, models.CathyID, models.ZendeskID, flows.TicketUUID(uuids.New()), "Old question", "Have you seen my cookies?", "34")
 
