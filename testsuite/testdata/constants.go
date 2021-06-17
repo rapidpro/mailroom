f8c90033d6b729f9e44b4e33a87a23b2b4ce0fd6
Rowan Seymour <rowanseymour@gmail.com>
2021-06-17 11:45:53 -0500

    Update system field test constants

--- testsuite/testdata/constants.go
+++ testsuite/testdata/constants.go
@@ -55,8 +55,8 @@ var IncomingExtraFlow = &Flow{10006, "376d3de6-7f0e-408c-80d6-b1919738bc80"}
 var ParentTimeoutFlow = &Flow{10007, "81c0f323-7e06-4e0c-a960-19c20f17117c"}
 var CampaignFlow = &Flow{10009, "3a92a964-3a8d-420b-9206-2cd9d884ac30"}
 
-var CreatedOnField = &Field{3, "f3bb7c36-8924-4a63-a42b-dfd83d754f2d"}
-var LastSeenOnField = &Field{5, "a15456d9-fb4c-4014-bbd8-831d93ccae22"}
+var CreatedOnField = &Field{3, "099bccaa-7853-4033-a066-c61c0ff32a8e"}
+var LastSeenOnField = &Field{5, "f72976e2-0fb8-491c-a4a0-ef344b2f72e0"}
 var GenderField = &Field{6, "3a5891e4-756e-4dc9-8e12-b7a766168824"}
 var AgeField = &Field{7, "903f51da-2717-47c7-a0d3-f2f32877013d"}
 var JoinedField = &Field{8, "d83aae24-4bbf-49d0-ab85-6bfd201eac6d"}
