0dda6831e83ec2e7c151fda1967e49dba392a875
Rowan Seymour <rowanseymour@gmail.com>
2022-01-17 15:00:44 -0500

    Add test for setting wait fields in case of subflow

--- core/models/runs_test.go
+++ core/models/runs_test.go
@@ -170,3 +170,82 @@ func TestSingleSprintSession(t *testing.T) {
 	assertdb.Query(t, db, `SELECT status, session_type, current_flow_id, responded FROM flows_flowsession`).
 		Columns(map[string]interface{}{"status": "C", "session_type": "M", "current_flow_id": nil, "responded": false})
 }
+
+func TestSessionWithSubflows(t *testing.T) {
+	ctx, rt, db, _ := testsuite.Get()
+
+	defer testsuite.Reset(testsuite.ResetData)
+
+	assetsJSON, err := os.ReadFile("testdata/session_test_flows.json")
+	require.NoError(t, err)
+
+	parentJSON, _, _, err := jsonparser.Get(assetsJSON, "flows", "[2]")
+	require.NoError(t, err)
+	testdata.InsertFlow(db, testdata.Org1, parentJSON)
+
+	childJSON, _, _, err := jsonparser.Get(assetsJSON, "flows", "[3]")
+	require.NoError(t, err)
+	childFlow := testdata.InsertFlow(db, testdata.Org1, childJSON)
+
+	oa, err := models.GetOrgAssetsWithRefresh(ctx, rt, testdata.Org1.ID, models.RefreshFlows)
+	require.NoError(t, err)
+
+	flowSession, sprint1 := test.NewSessionBuilder().WithAssets(assetsJSON).WithFlow("f128803a-9027-42b1-a707-f1dbe4cf88bd").
+		WithContact(testdata.Bob.UUID, flows.ContactID(testdata.Cathy.ID), "Cathy", "eng", "").MustBuild()
+
+	tx := db.MustBegin()
+
+	hookCalls := 0
+	hook := func(context.Context, *sqlx.Tx, *redis.Pool, *models.OrgAssets, []*models.Session) error {
+		hookCalls++
+		return nil
+	}
+
+	modelSessions, err := models.WriteSessions(ctx, rt, tx, oa, []flows.Session{flowSession}, []flows.Sprint{sprint1}, hook)
+	require.NoError(t, err)
+	assert.Equal(t, 1, hookCalls)
+
+	require.NoError(t, tx.Commit())
+
+	session := modelSessions[0]
+
+	assert.Equal(t, models.FlowTypeMessaging, session.SessionType())
+	assert.Equal(t, testdata.Cathy.ID, session.ContactID())
+	assert.Equal(t, models.SessionStatusWaiting, session.Status())
+	assert.Equal(t, childFlow.ID, session.CurrentFlowID())
+	assert.NotNil(t, session.CreatedOn())
+	assert.Nil(t, session.EndedOn())
+	assert.False(t, session.Responded())
+	assert.NotNil(t, session.WaitStartedOn())
+	assert.NotNil(t, session.WaitExpiresOn())
+	assert.True(t, *session.WaitResumeOnExpire()) // because we have a parent
+	assert.Nil(t, session.Timeout())
+
+	// check that matches what is in the db
+	assertdb.Query(t, db, `SELECT status, session_type, current_flow_id, responded, ended_on, wait_resume_on_expire FROM flows_flowsession`).
+		Columns(map[string]interface{}{
+			"status": "W", "session_type": "M", "current_flow_id": int64(childFlow.ID), "responded": false, "ended_on": nil, "wait_resume_on_expire": true,
+		})
+
+	flowSession, err = session.FlowSession(rt.Config, oa.SessionAssets(), oa.Env())
+	require.NoError(t, err)
+
+	flowSession, sprint2, err := test.ResumeSession(flowSession, assetsJSON, "yes")
+	require.NoError(t, err)
+
+	tx = db.MustBegin()
+
+	err = session.Update(ctx, rt, tx, oa, flowSession, sprint2, hook)
+	require.NoError(t, err)
+	assert.Equal(t, 2, hookCalls)
+
+	require.NoError(t, tx.Commit())
+
+	assert.Equal(t, models.SessionStatusCompleted, session.Status())
+	assert.Equal(t, models.NilFlowID, session.CurrentFlowID())
+	assert.True(t, session.Responded())
+	assert.Nil(t, session.WaitStartedOn())
+	assert.Nil(t, session.WaitExpiresOn())
+	assert.False(t, *session.WaitResumeOnExpire())
+	assert.Nil(t, session.Timeout())
+}
